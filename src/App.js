b6c7530e437783c805b1fac71520cf36fb668d2e
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-08-06 12:21:30 +1200

    Started the main page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    var baseUrl = "https://nicolaslferreira.github.io/";
+    var url = baseUrl + "Calendar";
+
+    return (
+        <div>
+            <nav class="navbar navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <p className="display-3 nav-item">Nicolas HUB</p>
+                </div>
+            </nav>
+        </div>
+    );
 }
 
 export default App;
